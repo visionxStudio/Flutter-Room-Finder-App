0478d369f04977f7d350ac984c084d5cd49c8739
officialvisionx <manish.rajak2055@gmail.com>
2021-12-09 23:45:08 +0545

    error text fixed in form field

--- lib/src/common/widgets/input_field/minimal_input_field.dart
+++ lib/src/common/widgets/input_field/minimal_input_field.dart
@@ -34,43 +34,78 @@ class _MinimalInputFieldState extends State<MinimalInputField> {
   FocusNode? focusNode;
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Flexible(
-            child: Container(
-              height: 50.0,
-              decoration: BoxDecoration(
-                  color: kWhiteColor, borderRadius: BorderRadius.circular(8.0)),
-              margin: const EdgeInsets.only(bottom: kDefaultPadding - 5),
-              child: TextFormField(
-                onChanged: widget.onChanged,
-                obscureText: widget.isPassword,
-                autofocus: widget.autofocus ?? false,
-                focusNode: widget.focusNode,
-                onTap: () {},
-                controller: widget.textEditingController,
-                maxLines: widget.isPassword ? 1 : widget.maxLines,
-                decoration: InputDecoration(
-                    hintText: widget.hintText,
-                    hintStyle: const TextStyle(
-                        color: ksearchText, fontWeight: FontWeight.normal),
-                    border: InputBorder.none,
-                    errorText: widget.errorText,
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    errorBorder: InputBorder.none,
-                    disabledBorder: InputBorder.none,
-                    contentPadding: const EdgeInsets.all(16.0)),
-                keyboardType: widget.textInputType ?? TextInputType.text,
-              ),
-            ),
-          ),
-        ],
+    // return SizedBox(
+    //   width: MediaQuery.of(context).size.width,
+    //   child: Row(
+    //     crossAxisAlignment: CrossAxisAlignment.center,
+    //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+    //     children: [
+    //       Flexible(
+    //         child: Container(
+    //           height: 50.0,
+    //           decoration: BoxDecoration(
+    //               color: kWhiteColor, borderRadius: BorderRadius.circular(8.0)),
+    //           margin: const EdgeInsets.only(bottom: kDefaultPadding - 5),
+    //           child: TextFormField(
+    //             onChanged: widget.onChanged,
+    //             obscureText: widget.isPassword,
+    //             autofocus: widget.autofocus ?? false,
+    //             focusNode: widget.focusNode,
+    //             onTap: () {},
+    //             controller: widget.textEditingController,
+    //             maxLines: widget.isPassword ? 1 : widget.maxLines,
+    //             decoration: InputDecoration(
+    //               hintText: widget.hintText,
+    //               hintStyle: const TextStyle(
+    //                   color: ksearchText, fontWeight: FontWeight.normal),
+    //               border: InputBorder.none,
+    //               errorText: widget.errorText,
+    //               focusedBorder: InputBorder.none,
+    //               enabledBorder: InputBorder.none,
+    //               errorBorder: InputBorder.none,
+    //               disabledBorder: InputBorder.none,
+    //               contentPadding: const EdgeInsets.all(16.0),
+    //             ),
+    //             keyboardType: widget.textInputType ?? TextInputType.text,
+    //           ),
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // );
+
+    return Container(
+      margin: const EdgeInsets.only(bottom: kDefaultPadding - 5),
+      child: TextFormField(
+        decoration: InputDecoration(
+          errorText: widget.errorText,
+          errorMaxLines: 1,
+          contentPadding: const EdgeInsets.all(16.0),
+          hintText: widget.hintText,
+          hintStyle: const TextStyle(
+              color: ksearchText, fontWeight: FontWeight.normal),
+          enabled: true,
+          enabledBorder: outlineInputBorder,
+          focusedBorder: outlineInputBorder,
+          border: outlineInputBorder,
+          fillColor: kWhiteColor,
+          filled: true,
+        ),
+        onChanged: widget.onChanged,
+        cursorColor: Colors.black,
+        cursorHeight: 20.0,
+        textInputAction: TextInputAction.next,
+        keyboardType: TextInputType.emailAddress,
+        onFieldSubmitted: (_) {},
       ),
     );
   }
 }
+
+final OutlineInputBorder outlineInputBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.circular(8),
+  borderSide: const BorderSide(
+    width: 0,
+    style: BorderStyle.none,
+  ),
+);
